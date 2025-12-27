e9f618f62cdc7e0411230180a42c2d073e0e6ac7
its-ipsa <itsipsa2023@gmail.com>
2025-12-27 19:14:28 +0530

    Connect frontend with AI prediction logic

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,23 @@
 function suggestSlot() {
+  // mock real-time inputs
+  const queueLength = 12;
+  const currentHour = new Date().getHours();
+
+  // AI prediction logic (same as backend for MVP)
+  let serviceTime = 2;
+  let timeFactor = 0;
+
+  if (currentHour >= 12 && currentHour <= 14) {
+    timeFactor = 10;
+  } else if (currentHour >= 17 && currentHour <= 19) {
+    timeFactor = 8;
+  }
+
+  const predictedWait =
+    (queueLength * serviceTime) + timeFactor;
+
   document.getElementById("slot").innerText =
-    "Recommended Arrival Time: 3:30 PM (Low Crowd)";
+    `Predicted Wait Time: ${predictedWait} minutes
+     | Suggested Arrival: ${predictedWait - 5} mins later`;
 }
+
